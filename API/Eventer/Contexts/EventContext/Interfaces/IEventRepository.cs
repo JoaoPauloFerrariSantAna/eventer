92a8a86ad2daccf897dd4ce28121756cd7cd910b
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-05-11 21:50:19 -0300

    feat: added interfaces

---
+++ API/Eventer/Contexts/EventContext/Interfaces/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using Eventer.Contexts.EventContext.DTOs.Requests;
+using Eventer.Contexts.EventContext.Entities;
+
+namespace Eventer.Contexts.EventContext.Interfaces
+{
+    public interface IEventRepository
+    {
+        public Event FindById(int Id);
+        public void Update(UpdateEventRequest updateEvent);
+    }
+}
